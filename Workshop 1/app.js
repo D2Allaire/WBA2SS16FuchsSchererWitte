b5e5868e21c8e89934f3850f304037570d2624a6
eugibob <eugen.scherer@smail.th-koeln.de>
2016-04-27 15:15:33 +0200

    aufgabe 1.3 geloest

--- Workshop 1/app.js
+++ Workshop 1/app.js
@@ -5,12 +5,28 @@ fs.readFile(__dirname+"/wolkenkratzer.json",  'utf8', function(err, data) {
     if (err) throw err;
     console.log("Parsing " + __dirname+"/wolkenkratzer.json");
     obj = JSON.parse(data);
-    
- 
-    for (var i=0; i<obj.wolkenkratzer.length; i++) {
-        console.log(chalk.blue("Name: " + obj.wolkenkratzer[i].name));
-        console.log(chalk.red("Stadt: " + obj.wolkenkratzer[i].stadt));
-        console.log(chalk.green("Höhe: " + obj.wolkenkratzer[i].hoehe));
-        console.log("----------------");
-}
+
+    // Array sortieren
+    obj.wolkenkratzer.sort(function(a,b){
+      return b.hoehe - a.hoehe;
+    })
+
+    	console.log();
+
+      // sortiert in neue Datei schreiben, vorher JS-Objekt wieder in JSON umwandeln
+      fs.writeFile(__dirname+"/wolkenkratzer_sortiert.json", JSON.stringify(obj.wolkenkratzer), function(err){
+      if(err) throw err;
+    })
+
+    console.log("Sortierte Ausgabe");
+
+      //Daten nochmals ausgeben
+  	for(var i = 0; i < obj.wolkenkratzer.length; i++){
+  		console.log(chalk.red('Name: '+obj.wolkenkratzer[i].name));
+  		console.log(chalk.green('Stadt: '+obj.wolkenkratzer[i].stadt));
+  		console.log(chalk.yellow('Hoehe: '+obj.wolkenkratzer[i].hoehe+'m'));
+  		console.log('-------------------------------------')
+  	}
+
+  	console.log();
 });
