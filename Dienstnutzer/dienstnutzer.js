dcfb1c3d847c1e9266efe7dc854672bd9eeba73a
eugibob <eugen.scherer@smail.th-koeln.de>
2016-06-13 19:25:12 +0200

    Dienstnutzer erweitert

--- Dienstnutzer/dienstnutzer.js
+++ Dienstnutzer/dienstnutzer.js
@@ -11,16 +11,40 @@ var getmovie;
 
 
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
-function (callback) {
-    unirest.get("localhost:3000/movies/")
-        .header("Accept", "application/json")
-        .header("Authorization", "Basic YWEyN2NiYzktMTY2Zi00N2MzLWE2NGUtYzIyN2Y4ZWM4M2ZiOjA3NjUxMGQ5LWU0ZjItNDkwNS04MGJkLWQxNTA5N2M0OWQ2MQ==")
-        .end(function (result) {
-            getmovie = result.body;
+app.get('/top', bodyParser.json(), function(req, res){
 
+
+    var options = {
+        host: 'localhost',
+        port: '3000',
+        path: '/movies/',
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Basic YWEyN2NiYzktMTY2Zi00N2MzLWE2NGUtYzIyN2Y4ZWM4M2ZiOjA3NjUxMGQ5LWU0ZjItNDkwNS04MGJkLWQxNTA5N2M0OWQ2MQ=='
+        }
+    };
+
+    var externalRequest = http.request(options, function(externalResponse){
+      externalResponse.on('data',function(chunk){
+
+        var data = JSON.parse(chunk);
+
+
+        console.dir(data);
+        res.render('index', {
+        data:data
         });
-},
+
+      });
+
+    });
+
+    externalRequest.end();
+
+});
 
 app.listen(3001);
 console.log("Port 3001 ist nun akiv");
